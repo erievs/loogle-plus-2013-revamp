fe54e150563bfe6ca38b26441945509a60226196
Erie Valley Software <65189871+erievs@users.noreply.github.com>
2024-07-21 19:47:15 -0400

    loogle main sql updated on github finnaly
    done

--- sql/loogle_main.sql
+++ sql/loogle_main.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 17, 2024 at 05:52 PM
--- Server version: 8.0.36-0ubuntu0.23.10.1
--- PHP Version: 8.2.10-2ubuntu1
+-- Generation Time: Jul 21, 2024 at 11:46 PM
+-- Server version: 8.0.37
+-- PHP Version: 8.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -38,6 +38,20 @@ CREATE TABLE `apiv1_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `bans`
+--
+
+CREATE TABLE `bans` (
+  `id` int NOT NULL,
+  `ip_address` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `reason` text NOT NULL,
+  `banned_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `comments`
 --
 
@@ -57,33 +71,59 @@ CREATE TABLE `comments` (
 
 CREATE TABLE `communities` (
   `community_id` int NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tagline` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `description` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   `members` int DEFAULT '0',
-  `members_list` text,
+  `members_list` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+  `image_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `creator_username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `display_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `links` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `communities_posts`
+--
+
+CREATE TABLE `communities_posts` (
+  `id` int NOT NULL,
+  `community_id` int DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `content` text,
   `image_url` varchar(255) DEFAULT NULL,
-  `creator_username` varchar(255) DEFAULT NULL,
-  `display_name` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `image_link_url` varchar(255) DEFAULT NULL,
+  `post_link_url` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `post_link` varchar(255) DEFAULT NULL,
+  `plus_one_usernames` varchar(255) DEFAULT NULL,
+  `plus_one` int DEFAULT NULL,
+  `post_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `communities`
+-- Table structure for table `community_comments`
 --
 
-INSERT INTO `communities` (`community_id`, `name`, `description`, `members`, `members_list`, `image_url`, `creator_username`, `display_name`) VALUES
-(1, 'f', '', -5, 'owner,member,member,member,d:member', '', 'd', 'eqeqqe'),
-(2, 'ffff_ffffff_', 'ff', 1, 'd:owner', '', 'd', 'fffff ffffff'),
-(3, 'Minecraft_Fan_Group', '', 1, 'd:owner', '', 'd', 'minecraft support forup'),
-(4, 'eqe', 'qe', 0, 'd:owner', NULL, 'd', 'eqe'),
-(5, 'XX', 'xx', 1, 'd:owner', NULL, 'd', 'XX'),
-(6, 'Minecraft Cool Group', 'minecraft cool group', 1, 'd:owner', NULL, 'd', 'Minecraft Cool Group'),
-(7, 'e', 'e', 1, 'd:owner', NULL, 'd', 'e'),
-(8, 'f', 'g', 1, 'd:owner', NULL, 'd', 'f'),
-(9, 'u', 'rr', 1, 'd:owner', NULL, 'd', 'u'),
-(10, 'bobevans', 'yum', 1, 'KSPortalcraft:owner', NULL, 'KSPortalcraft', 'bobevans'),
-(11, 'yest', 'ff', 1, 'KSPortalcraft:owner', NULL, 'KSPortalcraft', 'yest'),
-(12, 'ff', 'ff', 1, 'KSPortalcraft:owner', NULL, 'KSPortalcraft', 'ff'),
-(13, 'vvv', 'vvvv', 1, 'KSPortalcraft:owner', NULL, 'KSPortalcraft', 'vvv');
+CREATE TABLE `community_comments` (
+  `id` int NOT NULL,
+  `community_id` int DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `content` text,
+  `image_url` varchar(255) DEFAULT NULL,
+  `image_link_url` varchar(255) DEFAULT NULL,
+  `post_link_url` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `post_link` varchar(255) DEFAULT NULL,
+  `plus_one_usernames` varchar(255) DEFAULT NULL,
+  `plus_one` int DEFAULT NULL,
+  `post_id` int DEFAULT NULL,
+  `comment_content` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -98,33 +138,18 @@ CREATE TABLE `followers` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `followers`
+-- Table structure for table `ips`
 --
 
-INSERT INTO `followers` (`id`, `follower_id`, `following_id`, `created_at`) VALUES
-(366, 'KSPortalcraft', 'ben', '2023-08-31 23:19:26'),
-(368, 'KSPortalcraft', 'ben1', '2023-08-31 23:24:17'),
-(369, 'KSPortalcraft', 'KSPortalcraft', '2023-09-01 00:12:22'),
-(377, 'KSPortalcraft', 'dover', '2023-09-01 00:21:03'),
-(378, 'name', 'd', '2023-09-08 02:51:11'),
-(379, 'name', 'dover', '2023-09-08 02:51:13'),
-(380, 'name', 'ben', '2023-09-08 03:57:52'),
-(381, 'root', 'KSPortalcraft', '2023-09-09 05:04:13'),
-(382, 'root', 'd', '2023-09-09 05:04:14'),
-(383, 'root', 'Ben Dover', '2023-09-09 05:04:15'),
-(384, 'root', 'ben', '2023-09-09 05:04:15'),
-(385, 'root', 'ben1', '2023-09-09 05:04:16'),
-(386, 'root', 'root', '2023-09-09 05:04:17'),
-(388, 'root', 'bend', '2023-09-09 05:04:18'),
-(389, 'root', 'name', '2023-09-09 05:04:19'),
-(390, 'root', 'dover', '2023-09-09 05:04:20'),
-(391, 'root', 'ben3', '2023-09-09 05:04:21'),
-(392, 'root', 'kspc', '2023-09-09 05:04:22'),
-(393, 'd', 'Ben Dover', '2023-09-13 20:45:20'),
-(394, 'd', 'ben', '2023-09-13 20:45:20'),
-(396, 'd', 'name', '2023-09-13 20:59:18'),
-(403, 'd', 'dover', '2023-10-14 05:19:34');
+CREATE TABLE `ips` (
+  `id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -136,14 +161,6 @@ CREATE TABLE `moderators` (
   `id` int NOT NULL,
   `username` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `moderators`
---
-
-INSERT INTO `moderators` (`id`, `username`) VALUES
-(1, 'd'),
-(2, 'name');
 
 -- --------------------------------------------------------
 
@@ -161,14 +178,6 @@ CREATE TABLE `notifications` (
   `post_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `notifications`
---
-
-INSERT INTO `notifications` (`id`, `recipient`, `content`, `created_at`, `read_status`, `sender`, `post_id`) VALUES
-(9, 'd', 'Mentioned you in a post!', '2023-10-18 04:07:57', 1, 'd', 148),
-(10, 'd', 'Mentioned you in a post!', '2023-10-18 04:46:56', 1, 'd', 149);
-
 -- --------------------------------------------------------
 
 --
@@ -183,18 +192,10 @@ CREATE TABLE `posts` (
   `image_link_url` varchar(255) DEFAULT NULL,
   `post_link_url` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `post_link` varchar(255) DEFAULT NULL
+  `post_link` varchar(255) DEFAULT NULL,
+  `plus_one_usernames` varchar(255) DEFAULT NULL,
+  `plus_one` int DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `posts`
---
-
-INSERT INTO `posts` (`id`, `username`, `content`, `image_url`, `image_link_url`, `post_link_url`, `created_at`, `post_link`) VALUES
-(183, 'test', 'test', NULL, NULL, NULL, '2024-03-17 17:19:38', NULL),
-(184, 'test', 'test', NULL, NULL, NULL, '2024-03-17 17:19:42', NULL),
-(185, 'dff', 'ass', '', NULL, NULL, '2024-03-17 17:26:42', NULL),
-(186, 'dff', 'hi', '', NULL, NULL, '2024-03-17 17:26:46', NULL);
 
 -- --------------------------------------------------------
 
@@ -220,6 +221,12 @@ ALTER TABLE `apiv1_tokens`
   ADD KEY `user_id` (`user_id`);
 
 --
+-- Indexes for table `bans`
+--
+ALTER TABLE `bans`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
@@ -233,9 +240,28 @@ ALTER TABLE `communities`
   ADD PRIMARY KEY (`community_id`);
 
 --
+-- Indexes for table `communities_posts`
+--
+ALTER TABLE `communities_posts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `post_id` (`post_id`);
+
+--
+-- Indexes for table `community_comments`
+--
+ALTER TABLE `community_comments`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `followers`
 --
 ALTER TABLE `followers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ips`
+--
+ALTER TABLE `ips`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -270,49 +296,73 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `apiv1_tokens`
 --
 ALTER TABLE `apiv1_tokens`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `bans`
+--
+ALTER TABLE `bans`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `comment_id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `communities`
 --
 ALTER TABLE `communities`
-  MODIFY `community_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `community_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `communities_posts`
+--
+ALTER TABLE `communities_posts`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `community_comments`
+--
+ALTER TABLE `community_comments`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `followers`
 --
 ALTER TABLE `followers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=404;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `ips`
+--
+ALTER TABLE `ips`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `moderators`
 --
 ALTER TABLE `moderators`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=187;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
