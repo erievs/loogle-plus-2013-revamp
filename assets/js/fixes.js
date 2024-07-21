dee031b30e3b914b93bfa92ad6e27a1ff20d95ae
Erie Valley Software <65189871+erievs@users.noreply.github.com>
2024-07-21 19:45:24 -0400

    Add files via upload

--- assets/js/fixes.js
+++ assets/js/fixes.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
+
+$("img").each(function() {
+    console.log('Your mom');
+    if ($(this).attr("src") === "http://kspc.serv00.net/null") {
+        $(this).hide();
+    }
+});
+
 $("img").each(function() {
     console.log('Your mom');
     if ($(this).attr("src") === "https://kspc.serv00.net/null") {
         $(this).hide();
     }
 });
+
+$("img").each(function() {
+    console.log('Your mom');
+    if ($(this).attr("src") === "null") {
+        $(this).hide();
+    }
+});
+
 });
