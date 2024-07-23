4e1a193336da21916dc9e3764c6d20b8d7ab4cb7
Erie Valley Software <65189871+erievs@users.noreply.github.com>
2024-07-22 20:49:22 -0400

    Add files via upload

--- assets/js/fixes.js
+++ assets/js/fixes.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
-
-    function isV8Engine() {
-      return /Chrome|Chromium|Edg/.test(navigator.userAgent);
-    }
-  
-    if (isV8Engine()) {
       setInterval(function() {
         $('img[src="null"]').hide();
       }, 5); 
-    }
   });
