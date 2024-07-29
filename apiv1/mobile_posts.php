cd49d56e9a0efcb428a37c2f6eac094456804d94
Erie Valley Software <65189871+erievs@users.noreply.github.com>
2024-07-29 17:35:00 -0400

    Update mobile_posts.php

--- apiv1/mobile_posts.php
+++ apiv1/mobile_posts.php
@@ -37,25 +37,25 @@ function getPostsAndComments($username = null) {
             if ($commentsResult->num_rows > 0) {
                 while ($comment = $commentsResult->fetch_assoc()) {
                     $comments[] = array(
-                        'username' => htmlspecialchars($comment['username']),
-                        'comment_content' => htmlspecialchars($comment['comment_content'])
+                        'username' => $comment['username'],
+                        'comment_content' => $comment['comment_content']
                     );
                 }
             }
 
-            $image_url = !empty($post['image_url']) ? $siteurl . "/" . htmlspecialchars($post['image_url']) : null;
+            $image_url = !empty($post['image_url']) ? $siteurl . "/" . $post['image_url'] : null;
 
             $data[] = array(
                 'post' => array(
                     'id' => intval($post['id']),
-                    'username' => htmlspecialchars($post['username']),
-                    'content' => htmlspecialchars($post['content']),
+                    'username' => $post['username'],
+                    'content' => $post['content'],
                     'image_url' => $image_url,
-                    'post_link' => htmlspecialchars($post['post_link']),
-                    'post_link_url' => htmlspecialchars($post['post_link_url']),
+                    'post_link' => $post['post_link'],
+                    'post_link_url' => $post['post_link_url'],
                     'created_at' => $post['created_at'],
                     'plus_one' => intval($post['plus_one']),
-                    'plus_one_usernames' => htmlspecialchars($post['plus_one_usernames'])
+                    'plus_one_usernames' => $post['plus_one_usernames']
                 ),
                 'comments' => $comments
             );
@@ -70,5 +70,5 @@ function getPostsAndComments($username = null) {
 $username = isset($_GET['username']) ? $_GET['username'] : null;
 $data = getPostsAndComments($username);
 
-echo json_encode($data, JSON_PRETTY_PRINT | JSON_HEX_QUOT | JSON_HEX_APOS);
+echo json_encode($data, JSON_PRETTY_PRINT);
 ?>
