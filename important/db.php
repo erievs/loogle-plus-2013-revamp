cb3cfd7437eaac49cbcc939486e0516450c8509d
Erie Valley Software <65189871+erievs@users.noreply.github.com>
2024-08-22 13:22:05 -0400

    Update db.php
    made it so you can have multiple domains

--- important/db.php
+++ important/db.php
@@ -1,11 +1,21 @@
 <?php
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "nicknick";
-$db_name = "loogle_main";
+$db_host = "";
+$db_user = "";
+$db_pass = "";
+$db_name = "";
 
 $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
 
-$siteurl = $protocol . "://localhost:8090";
+$host = $_SERVER['HTTP_HOST'];
 
+if (strpos($host, 'loogle.cc') !== false) {
+    $siteurl = $protocol . "://loogle.cc";
+} elseif (strpos($host, 'plus.loogle.mooo') !== false) {
+    $siteurl = $protocol . "://plus.loogle.mooo";
+} else {
+
+    header('HTTP/1.0 400 Bad Request');
+    echo "Unrecognized domain.";
+    exit;
+}
 ?>
