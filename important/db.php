8b22309f46085c001aa4a05c7bd2c7653be766df
Erie Valley Software <65189871+erievs@users.noreply.github.com>
2024-08-23 09:40:09 -0400

    Update db.php

--- important/db.php
+++ important/db.php
@@ -1,21 +1,31 @@
 <?php
-$db_host = "";
+
+$db_host = "localhost";
 $db_user = "";
 $db_pass = "";
 $db_name = "";
 
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $protocol = 'https';  
+} elseif (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+    $protocol = 'https';  
+} else {
+    $protocol = 'http';   
+}
 
-$host = $_SERVER['HTTP_HOST'];
+$host = $_SERVER['HTTP_HOST'] ?? '';
 
 if (strpos($host, 'loogle.cc') !== false) {
-    $siteurl = $protocol . "://loogle.cc";
+    $siteurl = $protocol . "://loogle.cc";  
 } elseif (strpos($host, 'plus.loogle.mooo') !== false) {
-    $siteurl = $protocol . "://plus.loogle.mooo";
+    $siteurl = $protocol . "://plus.loogle.mooo";  
 } else {
 
     header('HTTP/1.0 400 Bad Request');
-    echo "Unrecognized domain.";
-    exit;
+    exit;  
 }
+
+$logFile = __DIR__ . '/server_log.txt';  
+$logContent = "Host: " . $host . "\nProtocol: " . $protocol . "\nSite URL: " . $siteurl . "\n";
+file_put_contents($logFile, $logContent, FILE_APPEND);  
 ?>
