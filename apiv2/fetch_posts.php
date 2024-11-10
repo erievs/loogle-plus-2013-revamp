4e16741ac9f9fd7c1f06f3d920e5c1aa008ee648
ksportalcraft <k@>
2024-11-10 18:39:25 -0500

    updated apiv2 fetch_posts to include comments

--- apiv2/fetch_posts.php
+++ apiv2/fetch_posts.php
@@ -18,7 +18,6 @@ function getPostsFromDatabase($username = null, $page = 1, $postsPerPage = 16, $
     $conn->set_charset("utf8");
 
     if ($disablePagination) {
-
         $query = "SELECT * FROM posts WHERE visibility = 'v' ORDER BY created_at DESC";
         if ($username !== null) {
             $stmt = $conn->prepare("SELECT * FROM posts WHERE visibility = 'v' AND username = ? ORDER BY created_at DESC");
@@ -27,9 +26,7 @@ function getPostsFromDatabase($username = null, $page = 1, $postsPerPage = 16, $
             $stmt = $conn->prepare($query);
         }
     } else {
-
         $offset = ($page - 1) * $postsPerPage;
-
         if ($username !== null) {
             $stmt = $conn->prepare("SELECT * FROM posts WHERE visibility = 'v' AND username = ? ORDER BY created_at DESC LIMIT ?, ?");
             $stmt->bind_param("sii", $username, $offset, $postsPerPage);
@@ -44,11 +41,12 @@ function getPostsFromDatabase($username = null, $page = 1, $postsPerPage = 16, $
     $posts = array();
 
     while ($post = $result->fetch_assoc()) {
-
         $image_url = !empty($post['image_url']) ? $siteurl . str_replace('..', '', $post['image_url']) : null;
         $post_url = !empty($post['post_link_url']) ? $post['post_link_url'] : null;
         $video_url = !empty($post['post_url']) ? $post['post_url'] : null;
         $plus_one = isset($post['plus_one']) ? $post['plus_one'] + 1 : 1;
+
+        $comments = getCommentsForPost($post['id']);
 
         $posts[] = array(
             'id' => $post['id'],
@@ -60,7 +58,8 @@ function getPostsFromDatabase($username = null, $page = 1, $postsPerPage = 16, $
             'post_url' => $video_url,
             'created_at' => $post['created_at'],
             'plus_one' => $plus_one,
-            'plus_one_usernames' => $post['plus_one_usernames']
+            'plus_one_usernames' => $post['plus_one_usernames'],
+            'comments' => $comments 
         );
     }
 
@@ -68,6 +67,45 @@ function getPostsFromDatabase($username = null, $page = 1, $postsPerPage = 16, $
     $conn->close();
 
     return $posts;
+}
+
+function getCommentsForPost($post_id) {
+    include("../important/db.php");
+
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+    $comments = array();
+
+    if ($conn->connect_error) {
+        return $comments; 
+    }
+
+    $conn->set_charset("utf8");
+
+    $sql = "SELECT * FROM comments WHERE post_id = ? ORDER BY created_at ASC"; 
+
+    if ($stmt = $conn->prepare($sql)) {
+        $stmt->bind_param("i", $post_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        while ($comment = $result->fetch_assoc()) {
+
+            $profile_image_url = $siteurl . "/apiv1/fetch_pfp_api.php?username=" . urlencode($comment['username']);
+
+            $comments[] = array(
+                'username' => $comment['username'],
+                'comment_content' => $comment['comment_content'],
+                'comment_time' => $comment['created_at'],
+                'profile_image_url' => $profile_image_url 
+            );
+        }
+
+        $stmt->close();
+    }
+
+    $conn->close();
+
+    return $comments;
 }
 
 $page = isset($_GET['page']) ? (int)$_GET['page'] : 1; 
